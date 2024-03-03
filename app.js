af87a28b958f68101b52934c28d0cf1145576a35
chalani <chalasaumya@gmail.com>
2024-03-03 19:05:24 +0530

    Completed

--- app.js
+++ app.js
@@ -1,68 +1,68 @@
-// console.log("Application Frameworks");
-// console.log("Lab 03");
+console.log("Application Frameworks");
+console.log("Lab 03");
 
 // 01 - read file
-// const fs = require('fs');
-// fs.readFile('file.txt', 'utf8', function (err, data) {
-//  if (err) throw err;
-//  console.log(data);
-// });
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
 
 // 02 - Write to file
-// const fs = require('fs');
-// fs.readFile('file.txt', 'utf8', function (err, data) {
-//  if (err) throw err;
-//  console.log(data);
-// });
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
 
 // 03 Creating a web server
-// const http = require('http');
-// http.createServer(function (req, res) {
-//  res.writeHead(200, {'Content-Type': 'text/html'});
-//  res.write('Hello World!');
-//  res.end();
-// }).listen(8080);
+const http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello World!');
+    res.end();
+}).listen(8080);
 
 // 04 Making an HTTP request
-// const https = require('https');
-// https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-//  let data = '';
-//  resp.on('data', (chunk) => {
-//  data += chunk;
-//  });
-//  resp.on('end', () => {
-//  console.log(JSON.parse(data));
-//  });
-// }).on('error', (err) => {
-//  console.log("Error: " + err.message);
-// });
+const https = require('https');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+    let data = '';
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+    resp.on('end', () => {
+        console.log(JSON.parse(data));
+    });
+}).on('error', (err) => {
+    console.log("Error: " + err.message);
+});
 
 // 05 Using a module
-// const myModule = require('./my-module.js');
-// console.log(myModule.myFunction());
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
 
-//06 Promises
-// const myPromise = new Promise((resolve, reject) => {
-//     if (condition) {
-//     resolve('Success!');
-//     } else {
-//     reject('Failure!');
-//     }
-//    });
-//    myPromise.then((result) => {
-//     console.log(result);
-//    }).catch((error) => {
-//     console.log(error);
-//    });
+// 06 Promises
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+        resolve('Success!');
+    } else {
+        reject('Failure!');
+    }
+});
+myPromise.then((result) => {
+    console.log(result);
+}).catch((error) => {
+    console.log(error);
+});
 
-// Async/Await
+Async / Await
 async function myFunction() {
     try {
-    const result = await myPromise;
-    console.log(result);
+        const result = await myPromise;
+        console.log(result);
     } catch (error) {
-    console.log(error);
+        console.log(error);
     }
-   }
-   myFunction();
-   
+}
+myFunction();
+
